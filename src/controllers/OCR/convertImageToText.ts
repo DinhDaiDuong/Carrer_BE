8f333b11894503e350dcac4c246d2159d48c9c01
Daiduong1593572468 <daiduong35578@gmail.com>
2024-12-19 10:07:56 +0700

    daidduong

--- src/controllers/OCR/convertImageToText.ts
+++ src/controllers/OCR/convertImageToText.ts
@@ -1,20 +1,20 @@
 import { Request, Response } from "express";
 import { createWorker } from "tesseract.js";
-
+import * as path from 'path';
+import * as fs from 'fs';
 export const convertImageToText = async (req: Request, res: Response) => {
-  const { imageUrl } = req.body;
-
-  if (!imageUrl) {
-    return res.status(400).json({ error: "Image URL is required" });
-  }
+  const imagePath = path.join(__dirname, '../../assets/img/bangdiem.jpg'); // Adjust the path to your image
   const { createWorker } = require('tesseract.js')
   const worker = await createWorker();
 
   try {
+    const image = fs.readFileSync(imagePath);
+    const imageBase64 = image.toString('base64');
+    const imageData = `data:image/jpeg;base64,${imageBase64}`;
     await worker.load();
     await worker.loadLanguage('vie');
     await worker.initialize('vie');
-    const { data: { text } } = await worker.recognize(imageUrl);
+    const { data: { text } } = await worker.recognize(imageData);
     await worker.terminate();
     return res.json({ text });
   } catch (error) {
